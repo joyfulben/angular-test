8f78f544e45805205dda06f96a33800ddcd63081
joyfulben <benfishernow@gmail.com>
2021-11-11 11:02:55 -0700

    Add HEROES in mock-heroes.ts; add element to each hero in HEROES; iterate through HEROES with *ngFor; add styles; add a click event binding with (click)="onSelect(hero)"; add event handler; add details section to heroes.component.html; add ngIf conditional to check if there is a selectedHero; add class binding with [class.selected];

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', element: 'air' },
+  { id: 12, name: 'Narco', element: 'earth' },
+  { id: 13, name: 'Bombasto', element: 'fire' },
+  { id: 14, name: 'Celeritas', element: 'water' },
+  { id: 15, name: 'Magneta', element: 'magma' },
+  { id: 16, name: 'RubberMan', element: 'air' },
+  { id: 17, name: 'Dynama', element: 'fire' },
+  { id: 18, name: 'Dr IQ', element: 'air' },
+  { id: 19, name: 'Magma', element: 'magma' },
+  { id: 20, name: 'Tornado', element: 'air' }
+];
