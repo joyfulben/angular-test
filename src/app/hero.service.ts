442d30f6993e5974281bb1bf3c98d331c060b40f
joyfulben <benfishernow@gmail.com>
2021-11-12 10:19:31 -0700

    Add delete hero button and functionality to heroes component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -50,6 +50,15 @@ updateHero(hero: Hero): Observable<any> {
   );
 }
 
+/** POST: add a new hero to the server */
+addHero(hero: Hero): Observable<Hero> {
+return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+  tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+  catchError(this.handleError<Hero>('addHero'))
+);
+}
+
+
 httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
